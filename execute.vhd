af5a849bd22012305fa9db7ff4d2dfbb635e695f
FawzY93 <m_fawzy93@hotmail.com>
2015-05-08 04:10:01 +0300

    undefine issue at simulation solution (2nd)

--- execute.vhd
+++ execute.vhd
@@ -40,6 +40,9 @@ architecture execute_arch of execute is
 	--1no_op & 1LS & 1sp & 2rd & 1MA& 9 bits non use  & 8s2 & 8result
   exmem_input(7 downto 0)<= result;
   exmem_input(15 downto 8)<=idex_output(15 downto 8);
+  ------------------------------
+  exmem_input(24 downto 16)<=(others=>'0');
+  ------------------------------
 
   -- prefere to pass the values in same postion
   exmem_input(25)<= idex_output(25);
@@ -47,7 +50,10 @@ architecture execute_arch of execute is
   exmem_input(28)<= idex_output(28);
   exmem_input(29)<= idex_output(29);
   exmem_input(30)<= idex_output(30);
-	
+	exmem_input(31)<= idex_output(31);
+  exmem_input(32)<= idex_output(32);
+  -----------------------------------------------------------------
+  exmem_input(40 downto 33)<=(others=>'0');
   -------------------------------------------------------------------
 	
   ALSU_module: ALSU port map(s1,s2,oper,cin,result,tmp_flags);
