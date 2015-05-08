e9be0166fcaf7d197676763184edeae68be54d6d
Ahmed Kamal <ahmed.kamal1432@gmail.com>
2015-05-08 23:06:09 +0200

    setup stall operation ondition and sp forwarding

--- decode.vhd
+++ decode.vhd
@@ -35,11 +35,15 @@ component Reg_file  is
    From_wb:in std_logic_vector(40 downto 0)
    );
 	end component;
-signal S1,S2,stack_value,inport_out,outport_out,reg_s1,reg_s2: std_logic_vector(7 downto 0);
+signal S1,S2,stack_value, stack_reg_value, inport_out,outport_out,reg_s1, reg_s2, pc_minus_1: std_logic_vector(7 downto 0);
 signal Rs1,Rs2	: std_logic_vector(1 downto 0);
+signal fetch_final,no_op_fetch: std_logic_vector(15 downto 0);
+signal opcode: std_logic_vector(3 downto 0);
+signal sp_hazard_in_sources, sources_in_execution_MA : std_logic;
 begin
  Rs2<=From_Fetch(1 downto 0);
  Rs1<=From_Fetch(3 downto 2);
+ opcode <= From_Fetch(7 downto 4);
 
   PC_In <= From_Fetch(7 downto 0) when From_Fetch(15 downto 8) = "00000000"
     else From_Fetch(15 downto 8) + 1;
@@ -47,7 +51,7 @@ begin
     -- forward implement
         -- from excute
     S1 <= Forward_from_execute(7 downto 0) when Forward_from_execute(27 downto 26) = Rs1 and Forward_from_execute(31) = '1'
-      --from MA and  it's MA operation
+      --from MA and it's MA operation
       else Forward_From_MA(7 downto 0) when Forward_From_MA(27 downto 26) = Rs1 and Forward_From_MA(31) = '1' and Forward_From_MA(25) = '1'
       -- from MA and it's NOT Ma operation 
       else Forward_From_MA(15 downto 8) when Forward_From_MA(27 downto 26) = Rs1 and Forward_From_MA(31) = '1' and Forward_From_MA(25) = '0'
@@ -65,11 +69,28 @@ begin
       else From_wb(7 downto 0) when From_wb(27 downto 26) = Rs2 and From_wb(31) = '1'
       --from register file      
       else reg_s2;
-        
+    
+      --sp equal to sp at MA when sp_flag at Ma  = 1
+    stack_value <= Forward_From_MA(15 downto 8) when Forward_From_MA(28) = '1'
+              -- sp equal wb when ot's sp_flag = 1
+              else From_wb(15 downto 8) when From_wb(28) = '1'
+              else stack_reg_value;
 
+  CU_MODUL: cu port map(rst,S1,S2,in_port,stack_value,fetch_final,to_idex);
+  Reg_file_MODUL: Reg_file port map(notclk, rst, '1', Rs1, Rs2, reg_s1, reg_s2, stack_reg_value, From_wb);
 
-  CU_MODUL		 :cu port map(rst,S1,S2,in_port,stack_value,From_Fetch,to_idex);
-  Reg_file_MODUL :Reg_file port map(notclk,rst,'1',Rs1,Rs2,reg_s1,reg_s2,stack_value,From_wb);
+    --stall if one of my sources in execution and it's MA
+  sources_in_execution_MA <= '1' when (Forward_from_execute(27 downto 26) = RS1 or Forward_from_execute(27 downto 26) = RS1) and Forward_from_execute(31) = '1' and Forward_from_execute(25) = '1' 
+    else '0';
+    -- stall if push or pop or source(1 or 2) = r3 and sp_flag in excute = 1
+  sp_hazard_in_sources <= '1' when(( RS1 = "11" or RS2 = "11" or (opcode = "0111" and RS1(1) = '0')) and Forward_from_execute(28) = '1')
+      else '0';
+  
+  pc_minus_1 <= From_Fetch(15 downto 8) - 1;
+  no_op_fetch <= pc_minus_1&"00000000";
+
+  fetch_final <=  no_op_fetch when sources_in_execution_MA = '1' and sp_hazard_in_sources = '1'
+    else From_Fetch;
 
   -----------------forward from execute got 1WB(31),1 NOP(30),2 rd(27-26) , 1 MA(25) ,8 result(7-0)
   -----------------forward from MA got 1WB(31),1 NOP(30),2 RD(27-26),1 MA(25),1 sp(16), ALu or sp(15-8),result(7-0)
