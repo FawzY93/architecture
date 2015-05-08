56bd1fa6ee02b0851c03e1684cdff0875c940ff0
Ahmed Kamal <ahmed.kamal1432@gmail.com>
2015-05-08 17:42:48 +0200

    forward seems worked but not accurites flags

--- cpu.vhd
+++ cpu.vhd
@@ -31,8 +31,8 @@ architecture cpu_arch of cpu is
 		in_port:in std_logic_vector(7 downto 0);
 		to_idex	:out std_logic_vector(40 downto 0);
     PC_In: out std_logic_vector(7 downto 0);
-		Forward_from_execute:out std_logic_vector(31 downto 0);
-		Forward_From_MA:out std_logic_vector(31 downto 0)
+		Forward_from_execute:in std_logic_vector(31 downto 0);
+		Forward_From_MA:in std_logic_vector(31 downto 0)
 
 		);
 	end component;
@@ -42,7 +42,7 @@ architecture cpu_arch of cpu is
         exmem_input: out std_logic_vector(40 downto 0);
         in_flags: in std_logic_vector(3 downto 0);
         out_flags: out std_logic_vector(3 downto 0);
-        Forward_from_execute:out std_logic_vector(10 downto 0)
+        Forward_from_execute:out std_logic_vector(31 downto 0)
         );
 	end component;
 	component Memory_Access  is
@@ -67,7 +67,7 @@ signal notclk:std_logic;
   signal PC_In,PC_Out,PC_In_Fetch :std_logic_vector(7 downto 0);
   signal ifid_input,ifid_output :std_logic_vector(15 downto 0);
   signal ifid_input_temp,ifid_output_temp :std_logic_vector(15 downto 0);
-  signal Forward_from_execute:std_logic_vector(31 downto 0);
+  signal Forward_From_MA,Forward_from_execute:std_logic_vector(31 downto 0);
   signal idex_input,idex_output, exmem_input,exmem_output,memwb_input,memwb_output,WB_Out :std_logic_vector(40 downto 0);
   signal idex_input_temp,idex_output_temp, exmem_input_temp,exmem_output_temp,memwb_input_temp,memwb_output_temp,WB_Out_temp :std_logic_vector(40 downto 0);
   begin
@@ -89,7 +89,7 @@ signal notclk:std_logic;
   ------------------------------------DECODE----------------------------------------------
   ifid_output<=(others=>'0')when rst='1'
 	else ifid_output_temp;
-  Decode_MODULE:decode port map(notclk,rst,ifid_output,WB_Out,in_port,idex_input_temp, PC_In);
+  Decode_MODULE:decode port map(notclk,rst,ifid_output,WB_Out,in_port,idex_input_temp, PC_In, Forward_from_execute, Forward_From_MA);
   idex_input<=(others=>'0')when rst='1'
 	else idex_input_temp;
 
@@ -97,14 +97,14 @@ signal notclk:std_logic;
   idex_output<=(others=>'0')when rst='1'
 	else idex_output_temp;
   ------------------------------------EXECUTE----------------------------------------------
-  EXECUTE_MODULE:execute port map(idex_output,exmem_input_temp,FLAGS_OUT,FLAGS_IN);
+  EXECUTE_MODULE:execute port map(idex_output,exmem_input_temp,FLAGS_OUT,FLAGS_IN,Forward_from_execute);
   exmem_input<=(others=>'0')when rst='1'
 	else exmem_input_temp;
   EXMEM_REG_MODULE:my_nreg generic map(41) port map(clk, rst, '1', exmem_input, exmem_output_temp);
   exmem_output<=(others=>'0')when rst='1'
 	else exmem_output_temp;
   ------------------------------------MEMORY ACCESS----------------------------------------------
-  MEMORY_ACCESS_MODULE:Memory_Access port map(clk,exmem_output,memwb_input_temp);
+  MEMORY_ACCESS_MODULE:Memory_Access port map(clk,exmem_output,memwb_input_temp,Forward_From_MA);
   memwb_input<=(others=>'0')when rst='1'
 	else memwb_input_temp;
   MEMWB_REG_MODULE:my_nreg generic map(41) port map(clk, rst, '1', memwb_input, memwb_output_temp);
