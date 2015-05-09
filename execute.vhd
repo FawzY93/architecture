af38e21b6c75cf5178de7bd9bb662ecc5d6428c7
sharabassy <abdelrahman.elsharabassy@gmail.com>
2015-05-10 00:22:32 +0200

    added mem_flags and condition for out_flags in execute

--- execute.vhd
+++ execute.vhd
@@ -7,7 +7,9 @@ entity execute is
         in_flags: in std_logic_vector(3 downto 0);
         out_flags: out std_logic_vector(3 downto 0);
         Forward_from_execute:out std_logic_vector(31 downto 0);
-        PC_loader_ex :out std_logic
+        PC_loader_ex :out std_logic;
+		mem_flags : in std_logic_vector(4 downto 0)
+		--
         );
 end execute;
 
@@ -19,13 +21,15 @@ architecture execute_arch of execute is
      in_flags : in std_logic_vector(3 downto 0);
      outt :out std_logic_vector(7 downto 0);
      flags:out std_logic_vector(3 downto 0) --(0) OV ,(1) C ,(2) Z ,(3) N
+	 
      );
   end component;
 
-  signal oper,change_flags,tmp_flags : std_logic_vector(3 downto 0);
+  signal oper,change_flags,alu_flags : std_logic_vector(3 downto 0);
   signal cin, MA ,sp , LS,NOP: std_logic;
   signal result, s2, s1 : std_logic_vector(7 downto 0);
   signal Rd: std_logic_vector(1 downto 0);
+  signal temp_flags :std_logic_vector(3 downto 0);
   begin
 	  -- I/P
 	  --1no_op & 1LS & 1sp & 2rd & 1MA & 1cin &4change_flags & 4oper & 8s2 & 8s1
@@ -64,22 +68,28 @@ architecture execute_arch of execute is
   Forward_from_execute(31 downto 8)<=idex_output(31 downto 8);
   -------------------------------------------------------------------
 	
-  ALSU_module: ALSU port map(s1,s2,oper,cin,in_flags, result,tmp_flags);
+  ALSU_module: ALSU port map(s1,s2,oper,cin,in_flags, result,alu_flags);
 
   -------------------------------------------------------------------
 
-  out_flags(0) <= tmp_flags(0) when change_flags(0) = '1'
+  temp_flags(0) <= alu_flags(0) when change_flags(0) = '1'
     else in_flags(0);
 
   -- set carry and clear carry instruction will be in 100x without any write back
-  out_flags(1) <= '1' when oper = "1001"
+  temp_flags(1) <= '1' when oper = "1001"
     else '0' when oper = "1000"
-    else tmp_flags(1) when change_flags(1) = '1'
+    else alu_flags(1) when change_flags(1) = '1'
     else in_flags(1);
 
-  out_flags(2) <= tmp_flags(2) when change_flags(2) = '1'
+  temp_flags(2) <= alu_flags(2) when change_flags(2) = '1'
     else in_flags(2);
-  out_flags(3) <= tmp_flags(3) when change_flags(3) = '1'
+  temp_flags(3) <= alu_flags(3) when change_flags(3) = '1'
     else in_flags(3);
+	---------------
+	
+	out_flags<=mem_flags(3 downto 0) when mem_flags(4)='1'
+	else temp_flags;
+	
+	
 
 end execute_arch;
