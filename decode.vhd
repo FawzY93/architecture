810d2940ca39e723b474cfea42c1a88ada586c8b
Ahmed Kamal <ahmed.kamal1432@gmail.com>
2015-05-09 08:18:41 +0200

    seprate get operand in new component

--- decode.vhd
+++ decode.vhd
@@ -37,6 +37,18 @@ component Reg_file  is
    From_wb:in std_logic_vector(40 downto 0)
    );
 	end component;
+
+component get_operand is
+  port(
+        From_Fetch :in std_logic_vector(15 downto 0);
+        From_wb:in std_logic_vector(40 downto 0);
+        Forward_from_execute:in std_logic_vector(31 downto 0);
+        Forward_From_MA:in std_logic_vector(31 downto 0);
+        reg_s1, reg_s2, stack_reg_value, ea_imm:in std_logic_vector(7 downto 0);
+        S1, S2, stack_value:out std_logic_vector(7 downto 0)
+        );
+end component;
+
 signal S1,S2,stack_value, stack_reg_value, inport_out,outport_out,reg_s1, reg_s2, pc_minus_1: std_logic_vector(7 downto 0);
 signal Rs1,Rs2	: std_logic_vector(1 downto 0);
 signal fetch_final,no_op_fetch: std_logic_vector(15 downto 0);
@@ -51,44 +63,19 @@ begin
   else From_Fetch(15 downto 8) when sources_in_execution_MA = '1' or sp_hazard_in_sources = '1'
     else From_Fetch(15 downto 8) + 1;
     
-    -- forward implement
-        -- from excute
-    S1 <= Forward_from_execute(7 downto 0) when Forward_from_execute(27 downto 26) = Rs1 and Forward_from_execute(31) = '1' and opcode /="1100"
-      --from MA and it's MA operation
-      else Forward_From_MA(7 downto 0) when Forward_From_MA(27 downto 26) = Rs1 and Forward_From_MA(31) = '1' and Forward_From_MA(25) = '1' and opcode /="1100"
-      -- from MA and it's NOT Ma operation 
-      else Forward_From_MA(15 downto 8) when Forward_From_MA(27 downto 26) = Rs1 and Forward_From_MA(31) = '1' and Forward_From_MA(25) = '0' and opcode /="1100"
-      -- from WA 
-      else From_wb(7 downto 0) when From_wb(27 downto 26) = Rs1 and From_wb(31) = '1' and opcode /="1100"
-      --LDM LDD STD
-      else ea_imm when opcode ="1100"
-      --from register file      
-      else reg_s1;
-        
-    S2 <= Forward_from_execute(7 downto 0) when Forward_from_execute(27 downto 26) = Rs2 and Forward_from_execute(31) = '1'
-      --from MA and  it's MA operation
-      else Forward_From_MA(7 downto 0) when Forward_From_MA(27 downto 26) = Rs2 and Forward_From_MA(31) = '1' and Forward_From_MA(25) = '1'
-      -- from MA and it's NOT Ma operation 
-      else Forward_From_MA(15 downto 8) when Forward_From_MA(27 downto 26) = Rs2 and Forward_From_MA(31) = '1' and Forward_From_MA(25) = '0'
-      -- from WA 
-      else From_wb(7 downto 0) when From_wb(27 downto 26) = Rs2 and From_wb(31) = '1'
-      --from register file      
-      else reg_s2;
-    
-      --sp equal to sp at MA when sp_flag at Ma  = 1
-    stack_value <= Forward_From_MA(15 downto 8) when Forward_From_MA(28) = '1'
-              -- sp equal wb when ot's sp_flag = 1
-              else From_wb(15 downto 8) when From_wb(28) = '1'
-              else stack_reg_value;
 
   CU_MODUL: cu port map(rst,S1,S2,in_port,stack_value,fetch_final,to_idex);
+
   Reg_file_MODUL: Reg_file port map(notclk, rst, '1', Rs1, Rs2, reg_s1, reg_s2, stack_reg_value, From_wb);
 
+    -- forward implement
+  GET_OPERAND_MODUL: get_operand port map(From_Fetch, From_wb, Forward_from_execute, Forward_From_MA, reg_s1, reg_s2, stack_reg_value, ea_imm, S1, S2, stack_value);
+
     --stall if one of my sources in execution and it's MA
-  sources_in_execution_MA <= '1' when (Forward_from_execute(27 downto 26) = RS1 or Forward_from_execute(27 downto 26) = RS2) and Forward_from_execute(31) = '1' and Forward_from_execute(25) = '1' 
+  sources_in_execution_MA <= '1' when (Forward_from_execute(27 downto 26) = Rs1 or Forward_from_execute(27 downto 26) = Rs2) and Forward_from_execute(31) = '1' and Forward_from_execute(25) = '1' 
     else '0';
     -- stall if push or pop or source(1 or 2) = r3 and sp_flag in excute = 1
-  sp_hazard_in_sources <= '1' when(( RS1 = "11" or RS2 = "11" or (opcode = "0111" and RS1(1) = '0')) and Forward_from_execute(28) = '1')
+  sp_hazard_in_sources <= '1' when(( Rs1 = "11" or Rs2 = "11" or (opcode = "0111" and Rs1(1) = '0')) and Forward_from_execute(28) = '1')
       else '0';
   From_decode<='1' when opcode="1100"
   else '0';
---
+++ get_operand.vhd
@@ -0,0 +1,51 @@
+Library ieee;
+use ieee.std_logic_1164.all;
+entity get_operand is
+  port(
+        From_Fetch :in std_logic_vector(15 downto 0);
+        From_wb:in std_logic_vector(40 downto 0);
+        Forward_from_execute:in std_logic_vector(31 downto 0);
+        Forward_From_MA:in std_logic_vector(31 downto 0);
+        reg_s1, reg_s2, stack_reg_value, ea_imm:in std_logic_vector(7 downto 0);
+        S1, S2, stack_value:out std_logic_vector(7 downto 0)
+        );
+end get_operand;
+
+architecture get_operand_arch of get_operand is
+  signal Rs1, Rs2: std_logic_vector(1 downto 0);
+  signal opcode: std_logic_vector(3 downto 0);
+  begin
+    Rs2<=From_Fetch(1 downto 0);
+    Rs1<=From_Fetch(3 downto 2);
+    opcode <= From_Fetch(7 downto 4);
+      
+      -- from excute
+      S1 <= Forward_from_execute(7 downto 0) when Forward_from_execute(27 downto 26) = Rs1 and Forward_from_execute(31) = '1' and opcode /="1100"
+      --from MA and it's MA operation
+      else Forward_From_MA(7 downto 0) when Forward_From_MA(27 downto 26) = Rs1 and Forward_From_MA(31) = '1' and Forward_From_MA(25) = '1' and opcode /="1100"
+      -- from MA and it's NOT Ma operation 
+      else Forward_From_MA(15 downto 8) when Forward_From_MA(27 downto 26) = Rs1 and Forward_From_MA(31) = '1' and Forward_From_MA(25) = '0' and opcode /="1100"
+      -- from WA 
+      else From_wb(7 downto 0) when From_wb(27 downto 26) = Rs1 and From_wb(31) = '1' and opcode /="1100"
+      --LDM LDD STD
+      else ea_imm when opcode ="1100"
+      --from register file      
+      else reg_s1;
+        
+    S2 <= Forward_from_execute(7 downto 0) when Forward_from_execute(27 downto 26) = Rs2 and Forward_from_execute(31) = '1'
+      --from MA and  it's MA operation
+      else Forward_From_MA(7 downto 0) when Forward_From_MA(27 downto 26) = Rs2 and Forward_From_MA(31) = '1' and Forward_From_MA(25) = '1'
+      -- from MA and it's NOT Ma operation 
+      else Forward_From_MA(15 downto 8) when Forward_From_MA(27 downto 26) = Rs2 and Forward_From_MA(31) = '1' and Forward_From_MA(25) = '0'
+      -- from WA 
+      else From_wb(7 downto 0) when From_wb(27 downto 26) = Rs2 and From_wb(31) = '1'
+      --from register file      
+      else reg_s2;
+    
+      --sp equal to sp at MA when sp_flag at Ma  = 1
+    stack_value <= Forward_From_MA(15 downto 8) when Forward_From_MA(28) = '1'
+              -- sp equal wb when ot's sp_flag = 1
+              else From_wb(15 downto 8) when From_wb(28) = '1'
+              else stack_reg_value;
+
+end get_operand_arch;
