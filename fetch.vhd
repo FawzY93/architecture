dcecf9a6c9d45ce81c0ccd2d2d7a4229caefed44
FawzY93 <m_fawzy93@hotmail.com>
2015-05-11 16:05:28 +0300

    try to change some issue for synopthis

--- fetch.vhd
+++ fetch.vhd
@@ -18,12 +18,13 @@ architecture f_arch of fetch is
 --test
 	signal ram : ram_type ;
 	signal dataout: std_logic_vector(7 downto 0); --instruction
-	signal push_pc,restor_flags:std_logic;
+	signal push_pc,restor_flags,prev:std_logic;
 	begin
 	restor_flags<='1' when dataout(7 downto 2)="101111"
 	else '0';
 
 	Inst_pc(15 downto 8)<= PC;
+	-------------------------------------------------
 	Inst_pc(7 downto 0)<= "10110100" when push_pc='1'         --INT
 	else "00000000" when temp_stall='1'      --INT
 	else "01110000" when save_flags='1'		 --INT
@@ -31,25 +32,35 @@ architecture f_arch of fetch is
 	else "10111000" when pop_pc='1'			 --RTI
 	else dataout when From_decode='0'
 	else (others=>'0');
+    -------------------------------------------------
+
 	Inst_pc(16)<=push_pc;
+	-------------------------------------------------
 	Inst_pc(17)<='1' when save_flags='1'
 	else '0';
+	-------------------------------------------------
 	Inst_pc(18)<=restor_flags;
 	ea_imm<=dataout;
+	-------------------------------------------------
 	Inst_pc(26 downto 19)<=dataout;
+	-------------------------------------------------
 	Inst_pc(27)<= '1' when temp_stall='1'
 	else '0';
+	-------------------------------------------------
 	Inst_pc(31 downto 28)<=(others=>'0');
+	-------------------------------------------------	
+
 		process(clk,Intr) is
 		  Begin
+		  	if Intr='1' and prev/='1' then
+		  		push_pc<='1';
+		  	else push_pc<='0';	
+ 			end if;
 			if falling_edge(clk) then 
 				dataout <= ram(to_integer(unsigned(PC)));	
+				prev<=Intr;
 			end if;
-			if rising_edge(Intr) then 
- 				push_pc<='1';
- 			elsif rising_edge(clk) then
- 				push_pc<='0';
- 			end if;
+				
 			
 		end process;
 
